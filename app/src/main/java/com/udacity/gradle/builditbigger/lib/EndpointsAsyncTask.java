3a0ea57cd8b0bbd8b3362fec24f2242c3f67a52a
Samir Moreira Antonio <samir.ma@gmail.com>
2016-08-27 23:32:53 -0300

    Async done

--- app/src/main/java/com/udacity/gradle/builditbigger/lib/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/lib/EndpointsAsyncTask.java
@@ -1,10 +1,7 @@
 package com.udacity.gradle.builditbigger.lib;
 
-import android.content.Context;
 import android.os.AsyncTask;
-import android.util.Pair;
-import android.widget.Toast;
-
+import android.util.Log;
 
 import com.example.samir.myapplication.backend.udacityApi.UdacityApi;
 import com.example.samir.myapplication.backend.udacityApi.model.JokeModel;
@@ -15,12 +12,20 @@ import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
-public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+public class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
     private static UdacityApi myApiService = null;
-    private Context context;
+
+    private EndpointsAsyncTaskDelegate delegate;
+
+    public EndpointsAsyncTask(EndpointsAsyncTaskDelegate delegate) {
+        this.delegate = delegate;
+    }
 
     @Override
-    protected String doInBackground(Pair<Context, String>... params) {
+    protected String doInBackground(Void... params) {
+
+        String jokeString = null;
+
         if(myApiService == null) {  // Only do this once
             UdacityApi.Builder builder = new UdacityApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
@@ -34,13 +39,10 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
                         abstractGoogleClientRequest.setDisableGZipContent(true);
                     }
                 });
-                // end options for devappserver
+            // end options for devappserver
 
             myApiService = builder.build();
         }
-
-        context = params[0].first;
-        String name = params[0].second;
 
         try {
             final UdacityApi.Joke joke = myApiService.joke();
@@ -48,12 +50,16 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
             final String data = execute.getData();
             return data;
         } catch (IOException e) {
-            return e.getMessage();
+            Log.e(EndpointsAsyncTask.class.getSimpleName(), e.getMessage(), e);
         }
+
+        return jokeString;
+
     }
+
 
     @Override
     protected void onPostExecute(String result) {
-        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        delegate.setJoke(result);
     }
 }
